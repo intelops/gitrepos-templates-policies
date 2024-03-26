5dc6ffefec19c789764cfd857a0c4d3554fd7afb
Santosh Kaluskar <ksantosh@intelops.dev>
2024-03-26 17:18:16 +0530

    enhance gisign script to check availability of tlog index
    Signed-off-by: Santosh Kaluskar <ksantosh@intelops.dev>

--- scripts/check_gitsign.sh
+++ scripts/check_gitsign.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Pre-commit hook to check if the latest commit is signed using gitsign
+# Pre-commit hook to check if last commit was signed using gitsign and contains a valid tlog index
 
 # Get the hash of the latest commit
 latest_commit=$(git rev-parse HEAD)
 
-# Check if the latest commit is signed
-signed_commit=$(git verify-commit $latest_commit 2>&1)
-
-# Check for a valid signature
-if [[ $signed_commit == *"Validated Git signature: true"* ]]; then
-    echo "Latest commit is signed with gitsign."
+# Check if the latest commit is signed and contains a valid tlog index
+if [[ $(git verify-commit $latest_commit 2>&1) == *"Validated Git signature: true"* && $(git verify-commit $latest_commit 2>&1) == *"tlog index: "*[0-9]* ]]; then
+    echo "Latest commit is signed with gitsign and contains a valid tlog index."
+    exit 0
 else
-    echo "WARNING: The latest commit is not signed with gitsign."
-    # Optionally, you can uncomment the next line to block the commit
+    echo "WARNING: The latest commit is either not signed with gitsign or does not contain a valid tlog index."
     exit 1
 fi
 
